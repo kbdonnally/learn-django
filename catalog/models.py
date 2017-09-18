679f2a0fbd8ca2248cb2886c90918a9789fbb3d0
Katherine <kbd3ax@virginia.edu>
2017-09-18 18:34:23 -0400

    comment on Author: add url handler from reverse()

--- catalog/models.py
+++ catalog/models.py
@@ -60,7 +60,7 @@ class Author(models.Model):
 	date_of_death = models.DateField('Died', null=True, blank=True) # 1st arg = verbose_name
 
 	def get_absolute_url(self):
-		return reverse('author-details', args=[str(self.id)])
+		return reverse('author-details', args=[str(self.id)]) # need to create URL handler 'author-details'
 
 	def __str__(self):
 		return '{0}, {1}'.format(self.last_name, self.first_name)
