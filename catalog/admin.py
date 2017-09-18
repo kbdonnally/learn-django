0415dcf49d5d5a9abf221cf9449ccc6e548169f7
Katherine <kbd3ax@virginia.edu>
2017-09-18 19:26:31 -0400

    registered 5/5 existing models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+# admin site config
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book, BookInstance, Author, Genre, Language
+
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
