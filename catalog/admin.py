a89cc6f600451716d2c1f3e83116e63a53274255
Katherine <kbd3ax@virginia.edu>
2017-09-18 20:48:58 -0400

    add ModelClass classes, register

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,8 +3,30 @@
 from django.contrib import admin
 from .models import Book, BookInstance, Author, Genre, Language
 
+''' Original:
 admin.site.register(Book)
 admin.site.register(BookInstance)
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Language)
+'''
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+	pass
