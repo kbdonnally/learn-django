b14d74c9cd0ee36e765ab94db77d958dcf96dd58
Katherine <kbd3ax@virginia.edu>
2017-09-22 18:52:59 -0400

    create: author list URL map, author list view, author list template doc, author detail template doc

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^books/$', views.BookListView.as_view(), name='books'),
 	url(r'^books/(?P<slug>[-\w]+)$', views.BookDetailView.as_view(), name='book-detail'), # pk -> slug; d+ -> [-\w]+
+	url(r'^authors/$', views.AuthorListView.as_view(), name='authors')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -21,3 +21,6 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
 	model = Book
+
+class AuthorListView(generic.ListView):
+	model = Author
